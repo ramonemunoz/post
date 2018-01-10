b01c19250ca3799974c4369b5cd7ecebf4de4c7c
ramonemunoz <ramonmunoz1027@gmail.com>
2018-01-09 23:39:05 -0800

    Added state props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from "react";
-import ActionButton from "./components/ActionButton";
-import Upload from "./components/Upload";
-import "./App.css";
+import React, { Component } from 'react';
+import ActionButton from './components/ActionButton';
+import Upload from './components/Upload';
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ActionButton />
-        <Upload />
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.updateActionButton = this.updateActionButton.bind(this);
+		//get initial state
+		this.state = {
+			actionButton: {}
+		};
+	}
+
+	updateActionButton(actionButtonState) {
+		const actionButton = { ...this.state.actionButton };
+		actionButton['buttonstate'] = actionButtonState;
+		this.setState({ actionButton });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<ActionButton updateActionButton={this.updateActionButton} />
+				<Upload />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React from 'react';
 
 class ActionButton extends React.Component {
-  actionButton() {
-    console.log("Clicked");
-    // const container = document.querySelector(".plusSign");
-    // const uploadContainer = document.getElementById("upload");
-    // container.classList.toggle("button-active");
-    // uploadContainer.classList.toggle("show");
-    //Watch Video 13
-
-  }
-  render() {
-    return (
-      <div className="buttonContainer">
-        <button
-          onClick={this.actionButton}
-          className="actionButton"
-        >
-          <div className="plusSign">+</div>
-        </button>
-      </div>
-    );
-  }
+	actionButtonClicked() {
+		console.log('Clickeds');
+		const actionButtonState = {
+			status: 'clicked'
+		};
+		// const container = document.querySelector(".plusSign");
+		// const uploadContainer = document.getElementById("upload");
+		// container.classList.toggle("button-active");
+		// uploadContainer.classList.toggle("show");
+		//https://stackoverflow.com/questions/36403101/toggle-class-in-react/36404061
+		console.log(actionButtonState);
+		this.props.updateActionButton(actionButtonState);
+	}
+	render() {
+		return (
+			<div className="buttonContainer">
+				<button
+					ref={button => (this.button = button)}
+					onClick={() => this.actionButtonClicked()}
+					className="actionButton"
+				>
+					<div className="plusSign">+</div>
+				</button>
+			</div>
+		);
+	}
 }
 export default ActionButton;
