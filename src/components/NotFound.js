2866bfafb418e3f2a70bc15137865ffd7d21af99
Ramon Munoz <ramonmunoz1027@gmail.com>
2018-01-11 11:17:41 -0800

    Updated css and simple pages

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class NotFound extends React.Component {
-	render() {
-		return (
-			<div className="NotFound">
-                <p>NotFound Page</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class="container">
+        <div class="textContainer">
+          <h2>404</h2>
+          <p>Lost?</p>
+        </div>
+      </div>
+    );
+  }
 }
 export default NotFound;
